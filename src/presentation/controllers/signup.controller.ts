aab5458400c92b9fd3c7e4008ce20c63b2fac3fd
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-07-29 22:27:46 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -12,14 +12,19 @@ export class SignUpController {
    * @return {HttpResponse}
    */
   public handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'));
+    const requiredFields = ['name', 'email'];
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'));
-    }
-
-    return badRequest(new MissingParamError('unknown'));
+    return {
+      statusCode: 200,
+      body: {
+        message: 'Hello world',
+      },
+    };
   }
 }
