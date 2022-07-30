707322d8372f2655d8768af00affa655bc0e46bd
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-07-29 22:41:10 -0300

    fix: add JsDoc for http protocols

--- src/presentation/protocols/http.protocol.ts
+++ src/presentation/protocols/http.protocol.ts
@@ -1,8 +1,14 @@
+/**
+ * HTTP response protocol
+ */
 export interface HttpResponse {
   statusCode: number;
   body: any;
 }
 
+/**
+ * HTTP request protocol
+ */
 export interface HttpRequest {
   body?: any;
 }
