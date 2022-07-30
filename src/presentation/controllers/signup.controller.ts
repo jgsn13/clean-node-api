83c035c9f655223a852d5861d67bd2be49cd907c
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-07-29 21:14:35 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.controller.ts
@@ -0,0 +1,16 @@
+/**
+ * SignUp controller class
+ */
+export class SignUpController {
+  /**
+   * Handle the request body
+   * @param {any} httpRequest - http signup request
+   * @return {any}
+   */
+  public handle(httpRequest: any): any {
+    console.log(httpRequest);
+    return {
+      statusCode: 400,
+    };
+  }
+}
