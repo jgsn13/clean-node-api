91700222b5455feb98c6fceeb2e8ed253bff83b1
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-08-06 15:10:02 -0300

    fix: add explicit valid email to test returning true

--- src/utils/email-validator.adapter.spec.ts
+++ src/utils/email-validator.adapter.spec.ts
@@ -17,7 +17,7 @@ describe('EmailValidator Adapter', () => {
 
   test('Should return true if validator returns true', () => {
     const sut = new EmailValidatorAdapter();
-    const isValid = sut.isValid('invalid_email@mail.com');
+    const isValid = sut.isValid('valid_email@mail.com');
     expect(isValid).toBe(true);
   });
 });
