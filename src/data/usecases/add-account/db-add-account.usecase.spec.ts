6d2db899c93e4f86c8abf23c70ba9e3d6504d7cd
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-08-06 18:10:24 -0300

    refactor: move sut creation to a factory helper function

--- src/data/usecases/add-account/db-add-account.usecase.spec.ts
+++ src/data/usecases/add-account/db-add-account.usecase.spec.ts
@@ -1,22 +1,35 @@
 import { DbAddAccount } from './db-add-account.usecase';
+import { Encrypter } from '../../protocols/encrypter.protocol';
+
+interface SutTypes {
+  sut: DbAddAccount;
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  /**
+   * Encrypter class
+   */
+  class EncrypterStub {
+    /**
+     * Generates a hashed value from a string
+     * @param {string} value - string to be hashed
+     * @return {Promise<string>}
+     */
+    public async encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve('hashed_password'));
+    }
+  }
+
+  const encrypterStub = new EncrypterStub();
+  const sut = new DbAddAccount(encrypterStub);
+
+  return { sut, encrypterStub };
+};
 
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    /**
-     * Encrypter class
-     */
-    class EncrypterStub {
-      /**
-       * Generates a hashed value from a string
-       * @param {string} value - string to be hashed
-       * @return {Promise<string>}
-       */
-      public async encrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve('hashed_password'));
-      }
-    }
-    const encrypterStub = new EncrypterStub();
-    const sut = new DbAddAccount(encrypterStub);
+    const { sut, encrypterStub } = makeSut();
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
     const accountData = {
       name: 'valid_name',
