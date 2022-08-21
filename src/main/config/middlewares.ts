897b4d387b3be21c898844cfae9e126b50a9de7d
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-08-21 11:40:45 -0300

    feat: ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/body-parser.middleware';
 import { cors } from '../middlewares/cors.middleware';
+import { contentType } from '../middlewares/content-type.middleware';
 
 export default (app: Express): void => {
   app.use(bodyParser);
   app.use(cors);
+  app.use(contentType);
 };
