b3d7ab3008fab33d330ef7461d2a176f691a6d83
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-08-21 11:27:45 -0300

    feat: ensure CORS is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/body-parser.middleware';
+import { cors } from '../middlewares/cors.middleware';
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 };
