57c09668208ca65094470de51b73770afa5cbe08
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-08-27 18:35:03 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,12 @@
-import app from './config/app';
+import env from './config/env';
 
-app.listen(4001, () => console.log('Server running at http://localhost:4001'));
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo.helper';
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default;
+    app.listen(env.port, () =>
+      console.log(`Server running at http://localhost:${env.port}`)
+    );
+  })
+  .catch(console.error);
